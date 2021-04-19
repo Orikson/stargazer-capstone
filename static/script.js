71bf713938a6f074c213b30634522c1431bec0ac
Geoffrey Wu <geoffrey.feifei@gmail.com>
2021-04-19 11:56:30 -0500

    display star centers

--- static/script.js
+++ static/script.js
@@ -17,12 +17,14 @@ fileSelector.addEventListener("change", (event) => {
         // Create the canvases to store the images
         var canvas = document.getElementById('canvas');
         var ctx = canvas.getContext('2d');
+        var canvasEdited = document.getElementById('canvasEdited');
+        var ctxEdited = canvasEdited.getContext('2d');
         img.onload = function () {
             // Set the dimensions of the canvases to fit the images
             canvas.width = img.width;
             canvas.height = img.height;
-            // canvasEdited.width = img.width;
-            // canvasEdited.height = img.height;
+            canvasEdited.width = img.width;
+            canvasEdited.height = img.height;
 
             // Display the original image
             ctx.drawImage(img, 0, 0);
@@ -36,19 +38,32 @@ fileSelector.addEventListener("change", (event) => {
             // You can try more different parameters
             cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
 
+            var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
             var star_centers = [];
             for (let i = 0; i < contours.size(); ++i) {
                 let cnt = cv.moments(contours.get(i), false);
-                let cx = cnt.m10 / cnt.m00;
-                let cy = cnt.m01 / cnt.m00;
+                let cx = Math.floor(cnt.m10 / cnt.m00);
+                let cy = Math.floor(cnt.m01 / cnt.m00);
                 star_centers.push([cx, cy]);
+
+                for (let j = -1; j <= 1; j++) {
+                    for (let k = -1; k <= 1; k++) {
+                        let index = (cx + j) + (cy + k) * canvas.width;
+                        index = 4 * index;
+                        imageData.data[index] = 255;
+                        imageData.data[index + 1] = 0;
+                        imageData.data[index + 2] = 255;
+                    }
+                }
             }
-            // cv.imshow('canvasEdited', dst);
+
             console.log(star_centers);
+
+            fetch(`/centers?arr=${encodeURI(star_centers)}`);
+
+            ctxEdited.putImageData(imageData, 0, 0);
             src.delete(); dst.delete(); contours.delete(); hierarchy.delete();
         }
     });
     reader.readAsDataURL(file);
 });
-
-
